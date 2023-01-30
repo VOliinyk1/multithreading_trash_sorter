bb1b03f97260793190ac0e4e9b373749c73e707a
VOliinyk1 <vov.25.98@gmail.com>
2023-01-30 10:15:31 +0200

    upd

--- sorter.py
+++ sorter.py
@@ -3,8 +3,8 @@ import os
 from threading import Thread
 import shutil
 
-WORK_FOLDER = path.join(r'E:\sort_trash\trash')
-OUTPUT_DIR = path.join(r'E:\sort_trash')
+WORK_FOLDER = path.join(r'trash')
+OUTPUT_DIR = path.join(r'sorted_trash')
 def sort_by_ext(file_path: path) -> None:
     _, folder_name = os.path.splitext(file_path)
     ext_folder = os.path.join(OUTPUT_DIR,folder_name)
@@ -16,14 +16,13 @@ def sort_files(folder_path: path) -> None:
     for file in os.listdir(folder_path):
         
         if os.path.isdir(os.path.join(folder_path, file)):
-            print(file, 'is dir')
             sort_files(os.path.join(folder_path, file))
         else:
             thread = Thread(name=f'{folder_path}', target=sort_by_ext, args=((os.path.join(folder_path,file),)))
             thread.start()
             threads.append(thread)
     [el.join() for el in threads]
-            # sort_by_ext(os.path.join(folder_path,file))
+            
 
 
 if __name__ == '__main__':
